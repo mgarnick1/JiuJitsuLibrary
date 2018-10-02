d9d3f15d1766fe098a67b69ac8d2a5f9668b2f9e
Mark Garnick <markgman008@gmail.com>
2018-10-01 20:21:21 -0600

    initial commit

---
+++ JiuJitsuLibrary/Users.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JiuJitsuLibrary
+{
+	public class Users
+	{
+		public Users()
+		{
+
+		}
+		private string fullName;
+
+		public string FullName
+		{
+			get { return fullName; }
+			set { fullName = value; }
+		}
+		private string emailAddress;
+
+		public string EmailAddress
+		{
+			get { return emailAddress; }
+			set { emailAddress = value; }
+		}
+
+
+	}
+}
---
+++ JiuJitsuLibrary/Videos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JiuJitsuLibrary
+{
+	public class Videos
+	{
+		public Videos()
+		{
+
+		}
+		private int videoNumber;
+
+		public int VideoNumber
+		{
+			get { return videoNumber; }
+			set { videoNumber = value; }
+		}
+		private string videoName;
+
+		public string VideoName
+		{
+			get { return videoName; }
+			set { videoName = value; }
+		}
+
+		private FileInfo filePath;
+
+		public FileInfo FilePath
+		{
+			get { return filePath; }
+			set { filePath = value; }
+		}
+		private string skillLevel;
+
+		public string SkillLevel
+		{
+			get { return skillLevel; }
+			set { skillLevel = value; }
+		}
+
+
+
+	}
+}
